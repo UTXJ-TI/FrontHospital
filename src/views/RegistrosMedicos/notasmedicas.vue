5f06ad1e67f93c87f3940c288d8f5f8b6cde3601
Jose Gomez <210562@utxicotepec.edu.mx>
2024-03-23 11:27:10 -0600

    funcionamiento de la tabla dinamica con datos estaticos.

--- src/views/RegistrosMedicos/notasmedicas.vue
+++ src/views/RegistrosMedicos/notasmedicas.vue
@@ -8,11 +8,12 @@
             <h4 class="card-title">Notas medicas</h4>
             <br />
             <div class="iq-search-bar">
-              <form action="#" class="searchbox">
+              <form action="#" class="searchbox" @submit.prevent="search">
                 <input
                   type="text"
                   class="text search-input"
                   placeholder="Buscar..."
+                  v-model="searchTerm"
                 />
                 <a class="search-link" href="#"
                   ><i class="ri-search-line"></i
@@ -32,20 +33,20 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="items in rows" :key="items">
-                    <td>{{ items.id }}</td>
-                    <td>{{ items.name }}</td>
-                    <td>{{ items.edad }}</td>
-                    <td>{{ items.nota }}</td>
+                  <tr v-for="item in filteredRows" :key="item.id">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.edad }}</td>
+                    <td>{{ item.nota }}</td>
                   </tr>
                 </tbody>
-                <tfoot>
+                <!--                 <tfoot>
                   <tr>
                     <th v-for="data in columns" :key="data">
                       {{ data.label }}
                     </th>
                   </tr>
-                </tfoot>
+                </tfoot> -->
               </table>
             </div>
           </template>
@@ -54,6 +55,7 @@
     </b-row>
   </b-container>
 </template>
+
 <script>
 import { xray } from "../../config/pluginInit";
 import iqCard from "../../components/xray/cards/iq-card";
@@ -102,38 +104,26 @@ export default {
           edad: "80",
           nota: "Necesita paracetamol cada 8 horas",
         },
-        {
-          id: 1,
-          name: "Tiger Nixon",
-          edad: "36",
-          nota: "Necesita paracetamol cada 8 horas",
-        },
-        {
-          id: 2,
-          name: "Garrett Winters",
-          edad: "24",
-          nota: "Necesita paracetamol cada 8 horas",
-        },
-        {
-          id: 3,
-          name: "Ashton Cox",
-          edad: "9",
-          nota: "Necesita paracetamol cada 8 horas",
-        },
-        {
-          id: 4,
-          name: "Cedric Kelly",
-          edad: "37",
-          nota: "Necesita paracetamol cada 8 horas",
-        },
-        {
-          id: 5,
-          name: "Airi Satou",
-          edad: "80",
-          nota: "Necesita paracetamol cada 8 horas",
-        },
       ],
+      searchTerm: "",
     };
+  },
+  computed: {
+    filteredRows() {
+      const regex = new RegExp(this.searchTerm.trim(), "i");
+      return this.rows.filter(
+        (item) =>
+          regex.test(item.id) ||
+          regex.test(item.name) ||
+          regex.test(item.edad) ||
+          regex.test(item.nota)
+      );
+    },
+  },
+  methods: {
+    search() {
+      // Puedes añadir aquí lógica adicional si lo necesitas
+    },
   },
 };
 </script>
