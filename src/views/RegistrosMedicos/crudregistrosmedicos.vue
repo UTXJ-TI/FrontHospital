0812f08c56cb8afec7e50a607cf0fdf767c8a01e
Jose Gomez <210562@utxicotepec.edu.mx>
2024-04-14 21:46:45 -0600

    realizacion del GET y DELETE de los documentos de ls base de datos

--- src/views/RegistrosMedicos/crudregistrosmedicos.vue
+++ src/views/RegistrosMedicos/crudregistrosmedicos.vue
@@ -9,35 +9,14 @@
           <template v-slot:body>
             <b-row>
               <div class="table-ad mb-3 me-2">
-                <b-button variant="btn btn-sm iq-bg-success float-end" @click="add">+ Add New</b-button>
+                <b-button
+                  variant="btn btn-sm iq-bg-success float-end"
+                  @click="add"
+                  >+ Add New</b-button
+                >
               </div>
               <b-col md="12" class="table-responsive w-100">
                 <b-table striped bordered hover :items="rows" :fields="columns">
-                  <template v-slot:cell(name)="data">
-                    <span v-if="!data.item.editable">{{ data.item.name }}</span>
-                    <input type="text" v-model="data.item.name" v-else class="form-control text-center" />
-                  </template>
-                  <template v-slot:cell(age)="data">
-                    <span v-if="!data.item.editable">{{ data.item.age }}</span>
-                    <input type="text" v-model="data.item.age" v-else class="form-control text-center" />
-                  </template>
-                  <template v-slot:cell(company_name)="data">
-                    <span v-if="!data.item.editable">{{ data.item.company_name }}</span>
-                    <input type="text" v-model="data.item.company_name" v-else class="form-control text-center" />
-                  </template>
-                  <template v-slot:cell(country)="data">
-                    <span v-if="!data.item.editable">{{ data.item.country }}</span>
-                    <input type="text" v-model="data.item.country" v-else class="form-control text-center" />
-                  </template>
-                  <template v-slot:cell(city)="data">
-                    <span v-if="!data.item.editable">{{ data.item.city }}</span>
-                    <input type="text" v-model="data.item.city" v-else class="form-control text-center" />
-                  </template>
-                  <template v-slot:cell(sort)>
-                    <td>
-                      <a href="#!" class="indigo-text"><i class="fa fa-long-arrow-up" aria-hidden="true"></i> <i class="fa fa-long-arrow-down ms-1" aria-hidden="true"></i></a>
-                    </td>
-                  </template>
                   <template v-slot:cell(remove)="data">
                     <b-button
                       variant=" iq-bg-success mr-1 mb-1"
@@ -53,7 +32,12 @@
                       v-else
                       >Ok</b-button
                     >
-                    <b-button variant=" iq-bg-danger" size="sm" @click="remove(data.item)">Remove </b-button>
+                    <b-button
+                      variant=" iq-bg-danger"
+                      size="sm"
+                      @click="remove(data.item)"
+                      >Remove
+                    </b-button>
                   </template>
                 </b-table>
               </b-col>
@@ -64,93 +48,58 @@
     </b-row>
   </b-container>
 </template>
+
 <script>
-import { xray } from '../../config/pluginInit'
-import iqCard from '../../components/xray/cards/iq-card'
+import iqCard from "../../components/xray/cards/iq-card";
+import apiService from "@/services/apiService";
+
 export default {
-  name: 'UiDataTable',
+  name: "UiDataTable",
   components: { iqCard },
   mounted() {
-    xray.index()
+    this.loadRecords();
   },
   methods: {
-    add() {
-      let obj = this.default()
-      this.rows.push(obj)
-    },
-    default() {
-      return {
-        id: this.rows.length,
-        name: '',
-        position: '',
-        office: '',
-        age: '',
-        start_date: '2011/04/25',
-        salary: '$0',
-        editable: false
+    async loadRecords() {
+      try {
+        const response = await apiService.getItems();
+        this.rows = response.data;
+      } catch (error) {
+        console.error("Error loading records:", error);
       }
     },
-    edit(item) {
-      item.editable = true
+    add() {
+      this.rows.push({
+        nombre: "",
+        edad: "",
+        notasM: "",
+      });
     },
-    submit(item) {
-      item.editable = false
+    async remove(item) {
+      try {
+        const response = await apiService.deleteItem(item._id);
+        if (response.status === 200) {
+          const index = this.rows.indexOf(item);
+          this.rows.splice(index, 1);
+        } else {
+          console.error("Error deleting record");
+        }
+      } catch (error) {
+        console.error("Error deleting record:", error);
+      }
     },
-    remove(item) {
-      let index = this.rows.indexOf(item)
-      this.rows.splice(index, 1)
-    }
   },
   data() {
     return {
       columns: [
-        { label: 'Name', key: 'name', class: 'text-left' },
-        { label: 'Age', key: 'age', class: 'text-left' },
-        { label: 'Company Name', key: 'company_name', class: 'text-left' },
-        { label: 'Country', key: 'country', class: 'text-left' },
-        { label: 'City', key: 'city', class: 'text-left' },
-        { label: 'Sort', key: 'sort', class: 'text-left' },
-        { label: 'Remove', key: 'remove', class: 'text-center' }
+        { key: "_id", label: "Id", headerClass: "text-left" },
+        { key: "nombre", label: "Nombre completo", headerClass: "text-left" },
+        { key: "edad", label: "Edad", headerClass: "text-left" },
+        { key: "notasM", label: "Nota", headerClass: "text-left" },
+        { key: "remove", label: "Remove", class: "text-center" },
       ],
-      rows: [
-        {
-          id: 1,
-          name: 'Gio Metric',
-          age: '25',
-          company_name: 'Deepends',
-          country: 'Spain',
-          city: 'Madrid',
-          editable: false
-        },
-        {
-          id: 1,
-          name: 'Manny Petty',
-          age: '45',
-          company_name: 'Insectus',
-          country: 'France',
-          city: 'San Francisco',
-          editable: false
-        },
-        {
-          id: 1,
-          name: 'Lucy Tania',
-          age: '26',
-          company_name: 'Isotronic',
-          country: 'Germany',
-          city: 'Frankfurt am Main',
-          editable: false
-        },
-        {
-          id: 1,
-          name: 'Anna Mull',
-          age: '35',
-          company_name: 'Portica',
-          country: 'USA',
-          city: 'Oregon',
-          editable: false
-        }
-      ]
-    }
-  }
-}
+      rows: [],
+    };
+  },
+};
 </script>
