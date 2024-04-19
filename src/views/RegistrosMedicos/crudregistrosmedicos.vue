5112ce6584737cefba5586ee598ba9e4055ead62
Jose Gomez <210562@utxicotepec.edu.mx>
2024-04-18 21:12:00 -0600

    activacion de la edicion de los documentos

--- src/views/RegistrosMedicos/crudregistrosmedicos.vue
+++ src/views/RegistrosMedicos/crudregistrosmedicos.vue
@@ -10,11 +10,33 @@
             <b-row>
               <b-col md="12" class="table-responsive w-100">
                 <b-table striped bordered hover :items="rows" :fields="columns">
+                  <template v-slot:cell(nombre)="data">
+                    <span v-if="!data.item.editable">{{
+                      data.item.nombre
+                    }}</span>
+                    <input
+                      type="text"
+                      v-model="data.item.nombre"
+                      v-else
+                      class="form-control text-center"
+                    />
+                  </template>
+                  <template v-slot:cell(notasM)="data">
+                    <span v-if="!data.item.editable">{{
+                      data.item.notasM
+                    }}</span>
+                    <input
+                      type="text"
+                      v-model="data.item.notasM"
+                      v-else
+                      class="form-control text-center"
+                    />
+                  </template>
                   <template v-slot:cell(remove)="data">
                     <b-button
                       variant=" iq-bg-success mr-1 mb-1"
                       size="sm"
-                      @click="edit(data.item)"
+                      @click="toggleEdit(data.item)"
                       v-if="!data.item.editable"
                       ><i class="ri-ball-pen-fill m-0"></i
                     ></b-button>
@@ -60,6 +82,7 @@ export default {
         this.rows = response.data.map((item) => ({
           ...item,
           nombreCompleto: `${item.nombre} ${item.primer_apellido} ${item.segundo_apellido}`,
+          editable: false, // Agregar la propiedad editable
         }));
       } catch (error) {
         console.error("Error loading records:", error);
@@ -71,8 +94,10 @@ export default {
         nombre: "",
         dob: "",
         notasM: "",
+        editable: true, // Nuevo registro es editable
       });
     },
+
     async remove(item) {
       try {
         const response = await apiService.deleteItem(item._id);
@@ -84,6 +109,20 @@ export default {
         }
       } catch (error) {
         console.error("Error deleting record:", error);
+      }
+    },
+
+    toggleEdit(item) {
+      item.editable = !item.editable; // Alternar entre editar y no editar
+    },
+
+    async submit(item) {
+      try {
+        // Aquí puedes enviar los cambios al servidor utilizando apiService.updateItem(item)
+        // Por simplicidad, aquí solo cambiaremos el estado editable a false
+        item.editable = false;
+      } catch (error) {
+        console.error("Error submitting changes:", error);
       }
     },
   },
